f09c50d5e5739164ad7e40fda246413104c4a413
Jessicavandermarel <vandermareljessica@gmail.com>
2017-07-12 10:07:37 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+// const Component = require("@angular/core");
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// module.exports = class AppComponent {...}
 export class AppComponent {
   title = 'app';
 }
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+// const Component = require("@angular.core").Component;
+// const OnInit = require("@angular.core").OnInit;
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+// module.exports = class FooterComponent ...
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
