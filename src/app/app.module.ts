1d5aeef5f08e45cfaa28c4a0d1d9332896abe678
Jessicavandermarel <vandermareljessica@gmail.com>
2017-07-12 12:59:59 -0400

    added form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MyCoolBindingsComponent } from './my-cool-bindings/my-cool-bindings.component';
 import { InputThangsComponent } from './input-thangs/input-thangs.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { InputThangsComponent } from './input-thangs/input-thangs.component';
     InputThangsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/input-thangs/input-thangs.component.ts
+++ src/app/input-thangs/input-thangs.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-thangs.component.css']
 })
 export class InputThangsComponent implements OnInit {
+  message: string = "Select an option";
+  // this variable is used to store the user's selection
+  mySelection: string;
+  // this variable is used to store the user's name (text-input)
+  userName: string;
+  // this variable is used to store the product info from the inputs
+  theProduct: object = {};
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  reactToSelection() {
+    if (this.mySelection === "bell") {
+      this.message = "You are being irresponsable with your body"
+    }
+    else if (this.mySelection === "ketchup") {
+      this.message = "Gross"
+    }
+    else {
+      this.message = "A fine selection"
+    }
+  }
+
+  createPoduct() {
+    console.log("Received user product!")
+    console.log(this.theProduct);
   }
 
 }
